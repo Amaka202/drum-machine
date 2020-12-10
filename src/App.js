2a74f3d808ecfa25a1cf28f731f65acd7f52290a
Amaka Umeh <amakaumeh01@gmail.com>
2020-12-10 09:25:11 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import "./App.css"
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
